d51898c82450d135cc3b4b02ab886f0991009588
sylvanor <adfor409@gmail.com>
2026-01-13 16:32:23 +0000

    Cambios realizados en clases post tarea terminada

---
+++ app/Http/Controllers/Api/ApiProductController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\product;
+use Illuminate\Http\Request;
+
+class ApiProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'status' => true,
+            'message' => 'Lista de productos en BD',
+            'data' => product::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(product $product)
+    {
+        //
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\Api\ApiProductController;
+use App\Http\Controllers\Api\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/status', function (){
+    return response()->json([
+        'status' => 'ok',
+        'laravel' => app()->version(),
+    ]);
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::apiResource('products', ApiProductController::class);    
+});
+
+Route::post('/login', [UserController::class, 'index']);
